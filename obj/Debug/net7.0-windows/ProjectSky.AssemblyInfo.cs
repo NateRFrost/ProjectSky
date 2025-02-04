a10754aaf6f16c951145723069634c1a798cdb37
NateRFrost <nathanielRfrost@gmail.com>
2025-02-03 16:03:05 -0800

    Folders that should be in git ignore

--- obj/Debug/net7.0-windows/ProjectSky.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows/ProjectSky.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectSky")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90e6cf489b8a782b7903610bc18c6e0b76cd4f5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b28ef11a5fa4f08101da9085e2f18e8c1532e6b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectSky")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectSky")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
